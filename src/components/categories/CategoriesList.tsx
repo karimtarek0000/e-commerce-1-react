aa9b86d6b18afce70bedafd0f9f7cedae7fe5c5c
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-04-17 18:28:05 +0200

    Categories

--- src/components/categories/CategoriesList.tsx
+++ src/components/categories/CategoriesList.tsx
@@ -21,7 +21,7 @@ function CategoriesList(): JSX.Element {
   ));
 
   return (
-    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+    <div className="row row-cols-1 row-cols-md-2 row-cols-xl-4 g-4">
       {loading ? <CategoriesLoading /> : cards}
     </div>
   );
