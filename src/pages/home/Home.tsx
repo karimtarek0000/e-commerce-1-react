ad3b2077a57383316a77ba2478d894fd042ffdd0
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-04-17 03:39:14 +0200

    Card

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,40 +6,9 @@ function Home() {
     <>
       {/* <Slider /> */}
       <Container className="mt-4">
-        <div className="row row-cols-4 gx-5 gy-4">
+        <div className="row row-cols-md-4 gx-5 gy-4">
           <div className="col card">
             <img
-              className="obj-fit"
-              src="https://res.cloudinary.com/dwp0imlbj/image/upload/v1680747343/Route-Academy-categories/1681511964020.jpeg"
-              alt=""
-              loading="lazy"
-            />
-            <div className="card__overlay"></div>
-            <h3 className="card__title">test</h3>
-          </div>
-          <div className="col card">
-            <img
-              className="obj-fit"
-              src="https://res.cloudinary.com/dwp0imlbj/image/upload/v1680747343/Route-Academy-categories/1681511964020.jpeg"
-              alt=""
-              loading="lazy"
-            />
-            <div className="card__overlay"></div>
-            <h3 className="card__title">test</h3>
-          </div>
-          <div className="col card">
-            <img
-              className="obj-fit"
-              src="https://res.cloudinary.com/dwp0imlbj/image/upload/v1680747343/Route-Academy-categories/1681511964020.jpeg"
-              alt=""
-              loading="lazy"
-            />
-            <div className="card__overlay"></div>
-            <h3 className="card__title">test</h3>
-          </div>
-          <div className="col card">
-            <img
-              className="obj-fit"
               src="https://res.cloudinary.com/dwp0imlbj/image/upload/v1680747343/Route-Academy-categories/1681511964020.jpeg"
               alt=""
               loading="lazy"
