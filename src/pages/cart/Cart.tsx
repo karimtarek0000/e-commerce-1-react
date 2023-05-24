a582eafc8c2d40bdd8a02e1df7161903770d5463
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-05-25 00:16:10 +0300

    Cart

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,11 +1,14 @@
 import { useSelector } from "react-redux";
 import { RootStateCart } from "../../types";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import CardCart from "../../components/cart/CardCart";
 import { ProductCart } from "../../types/store";
+import guard from "../../gurad/gurad";
 
 function Cart() {
-  const { products } = useSelector((state: RootStateCart) => state.cart);
+  const { products, numOfCartItems, totalCartPrice } = useSelector(
+    (state: RootStateCart) => state.cart
+  );
 
   const renderCardCart = products?.map((product: ProductCart) => {
     return <CardCart key={product._id} productCard={product} />;
@@ -13,9 +16,29 @@ function Cart() {
 
   return (
     <Container className="px-4 px-lg-0" style={{ minHeight: "70vh" }}>
+      {numOfCartItems > 0 && (
+        <Row className="mt-4">
+          <Col className="flex-start flex-column">
+            <h3 className="fs-2 mb-3">
+              Count:
+              <span className="bg-primary text-white px-3 py-1 rounded-1 ms-5">
+                {numOfCartItems}
+              </span>
+            </h3>
+            <h3 className="fs-2">
+              Total:
+              <span className="bg-primary text-white px-3 py-1 rounded-1 ms-5">
+                ${totalCartPrice.toLocaleString("en-US")}
+              </span>
+            </h3>
+          </Col>
+        </Row>
+      )}
+
+      {/* Cards */}
       <div className="mt-5">{renderCardCart}</div>
     </Container>
   );
 }
 
-export default Cart;
+export default guard(Cart);
