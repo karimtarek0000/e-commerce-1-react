5a92d49a8a6410e39c1d8de27915f11662423bd7
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-06-19 20:25:25 +0300

    Fix height category list

--- src/components/categories/CategorieCard.tsx
+++ src/components/categories/CategorieCard.tsx
@@ -9,7 +9,11 @@ function CategorieCard({
   type,
 }: CategorieCardTypes): JSX.Element {
   return (
-    <Link to={`products/${type}/${slug}/${id}`} className="col category-card">
+    <Link
+      to={`products/${type}/${slug}/${id}`}
+      className="col category-card"
+      style={{ minHeight: type === "category" ? "44.5rem" : "22.2rem" }}
+    >
       <img src={image} alt={name} loading="lazy" />
       <h3 className="category-card__title">
         {type === "category" ? name : ""}
